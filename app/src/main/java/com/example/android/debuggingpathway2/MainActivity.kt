4c56d0bfe47db0308d85e944c2c22be7c8c88d2a
himanatsuri <86057109+himanatsuri@users.noreply.github.com>
2023-02-02 14:16:48 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/android/debuggingpathway2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.android.debuggingpathway2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+public val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        division()
+    }
+
+    fun division() {
+        val numerator = 60
+        val denominator = 4
+        repeat(4) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+}
